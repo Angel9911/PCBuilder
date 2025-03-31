ab4aac1478543dbc0ff552a0ec03a75d16d5f9ad
unknown <angel_dimitrov99@abv.bg>
2025-04-01 02:36:21 +0300

    Add more vendors

--- src/Constraints/VendorConstraints.php
+++ src/Constraints/VendorConstraints.php
@@ -22,6 +22,18 @@ final class VendorConstraints
             'product_price_style' => '.prod-price .prices .price',
             'product_status_style' => '.sprp.available',
             'product_logo_style' => '#logo img'
+        ],
+        'JAR Computers' => [
+            'product_name_style' => '#product_name h1',
+            'product_price_style' => '.price-product',
+            'product_status_style' => '.av-label',
+            'product_logo_style' => '#logo .logo-image img' // Extract website logo
+        ],
+        'Ozone' => [
+            'product_name_style' => 'h1[itemprop="name"]', // Extracts the product name
+            'product_price_style' => '.regular-price .price', // Extracts the main price
+            'product_status_style' => '.availability.in-stock', // Checks for stock availability
+            'product_logo_style' => '.responsive-logo img' // Extracts website logo
         ]
     ];
 }
